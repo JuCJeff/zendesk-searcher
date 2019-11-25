d2ac32ad668e317e09a7ed1c3c550da8f160d50c
JuCJeff <zma67@wisc.edu>
2019-11-25 02:15:52 -0600

    Did sanity check of workflow based on README

--- frontend-web/src/main.js
+++ frontend-web/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
-import store from './store.js';
+import vuetify from './plugins/vuetify'
+import store from './store.js'
 
 Vue.config.productionTip = false
 
